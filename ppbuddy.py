cc3ba09b70a474201a4cb027f2a6b410b03ca233
grilo <joao.grilo@gmail.com>
2016-04-22 05:33:51 +0200

    Substantially improve the UI, fix the heuristics.

--- ppbuddy.py
+++ ppbuddy.py
@@ -12,6 +12,34 @@ import logging
 import argparse
 
 
+def colorize(color, string):
+    colors = {
+        'header': '\033[95m',
+        'blue': '\033[94m',
+        'green': '\033[92m',
+        'warning': '\033[93m',
+        'fail': '\033[91m',
+        'endcolor': '\033[0m',
+        'bold': '\033[1m',
+        'underline': '\033[4m',
+    }
+    return "%s%s%s" % (colors[color], string, colors['endcolor'])
+
+def check_expiration(date):
+    color = 'green'
+    warning_threshold = 1296000 # 2 weeks
+    critical_threshold = 64800000 # 1 week
+    delta = date - time.time()
+    s = ''
+    if  delta < critical_threshold:
+        logging.critical("Expiration date is very close!")
+        color = 'fail'
+    elif delta < warning_threshold:
+        logging.warning("Expiration date is coming soon...")
+        color = 'warning'
+    s += (colorize(color, time.ctime(date)))
+    return s
+
 def cert_decoder(cert_string):
     with tempfile.NamedTemporaryFile() as temp:
         temp.write(cert_string)
@@ -33,6 +61,7 @@ class Cert(object):
 
     def __init__(self, cert_string):
         self.validity, self.uid, self.cn = self._parse_cert(cert_string)
+        self.cn = self.cn.decode("unicode_escape").encode("latin1")
 
     def _parse_cert(self, cert):
         validity, uid, cn = None, None, None
@@ -52,20 +81,30 @@ class MobileProvision(object):
 
     def __init__(self, pp_dict, cert_decoder):
         self.uuid = pp_dict['UUID']
+        self.name = pp_dict['Name']
         self.appid = pp_dict['Entitlements']['application-identifier'].split('.', 1)[1]
         self.expiration_date = time.mktime(pp_dict['ExpirationDate'].timetuple())
-        devices_udids = pp_dict.get('ProvisionedDevices', None)
-        self.udids = devices_udids or []
-        self.is_appstore = (devices_udids is None)
+        self.udids = pp_dict.get('ProvisionedDevices', None) or []
+        # Determines whether the Xcode debugger can attacxh to the App
+        # A heuristic used to know if this is a distribution or a development
+        # provisioning profile
+        self.development = pp_dict['Entitlements']['get-task-allow']
+        self.pushnotifications = False
+        try:
+            self.apsenvironment = pp_dict['Entitlements']['aps-environment']
+        except KeyError:
+            pass
         self.certs = []
+        self.certs_expired = []
         for c in pp_dict['DeveloperCertificates']:
             cert = Cert(cert_decoder(c.data))
             if cert.validity <= time.time():
-                continue
-            self.certs.append(cert)
+                logging.debug("%s: Old certificate found." % (self.uuid))
+                self.certs_expired.append(cert)
+            else:
+                logging.debug("%s: Up-to-date certificate found." % (self.uuid))
+                self.certs.append(cert)
 
-    def __repr__(self):
-        return " :: ".join([self.appid.rjust(30), str(self.is_appstore).rjust(5), self.uuid.ljust(36), time.ctime(self.expiration_date), str(self.certs)])
 
 if __name__ == '__main__':
 
@@ -74,55 +113,78 @@ if __name__ == '__main__':
 
     parser.add_argument("-v", "--verbose", action="store_true", \
         help="Increase output verbosity")
-    parser.add_argument("-l", "--list", default=True, type=bool, \
-        help="List all available provisioning profiles and their relevant contents.")
     parser.add_argument("-a", "--appid", default=None, type=str, \
         help="The application ID to look for.")
-    parser.add_argument("-w", "--wildcard", type=bool, default=False, \
-        help="Wether wildcard provisioning profiles should be considered.")
-    parser.add_argument("-d", "--development", type=bool, \
+    parser.add_argument("-w", "--wildcard", action="store_false", \
+        help="Wether wildcard provisioning profiles should be prioritized when determining the best profile.")
+    parser.add_argument("-p", "--production", action="store_true", \
         help="Look for development certificates only (as opposed to production certificates).")
-    parser.add_argument("-k", "--keychain", type=str, \
-        help="The path to the keychain containing the certificates (improves the matching heuristic by allowing the inspection of codesigning certificates).")
-    parser.add_argument("-p", "--password", type=str, \
-        help="The password for the keychain (required to unlock it).")
-    parser.add_argument("-m", "--mobiledevices", type=str, default=os.path.join(os.path.expanduser('~'), "Library", "MobileDevices", "Provisioning Profiles"), \
+    parser.add_argument("-i", "--identities", type=str, \
+        help="The output of the /usr/bin/security find-identity -p codesigning -v <keychain> command. Improves the profile matching heuristic")
+    parser.add_argument("-m", "--mobiledevices", type=str, default=os.path.join(os.path.expanduser('~'), "Library", "MobileDevice", "Provisioning Profiles"), \
         help="The directory to look for installed provisioning profiles.")
+    parser.add_argument("-r", "--report", action="store_true", \
+        help="Generate a nice looking report instead of generating output for an application.")
 
     args = parser.parse_args()
 
     logging.basicConfig(format='%(asctime)s::%(levelname)s::%(message)s')
     logging.getLogger().setLevel(getattr(logging, 'INFO'))
-    logging.getLogger().addHandler(logging.StreamHandler())
     if args.verbose:
         logging.getLogger().setLevel(logging.DEBUG)
 
-    args.mobiledevices = "."
     provisioning_profiles = []
     for profile in os.listdir(args.mobiledevices):
-        if not os.path.isfile(profile): continue
+        pp_fullpath = os.path.join(args.mobiledevices, profile)
+        if not os.path.isfile(pp_fullpath): continue
         elif not profile.endswith("mobileprovision"): continue
 
-        try:
-            p = MobileProvision(pp_loader(profile), cert_decoder)
+        p = MobileProvision(pp_loader(pp_fullpath), cert_decoder)
 
-            if args.appid and args.appid != p.appid:
-                logging.debug("Doesn't match specified appid: %s" % (profile))
-                continue
-            if args.development == True and p.is_appstore:
-                logging.debug("Doesn't match environment: %s" % (profile))
-                continue
-            if args.wildcard and p.appid != "*":
-                logging.debug("It's not a wildcard: %s" % (profile))
-                continue
-            if not p.certs:
-                logging.debug("No valid certificates found: %s" % (profile))
-                continue
+        # When in report mode, just show the info
+        if args.report:
+            print colorize('blue', '------------------------------------------')
+            print "Name: %s" % (p.name)
+            print "AppId: %s" % (p.appid)
+            print "UUID: %s" % (p.uuid)
+            print "Development?: %s" % (p.development)
+            print "PushNotifications?: %s" % (p.pushnotifications)
+            print "Expiration: %s" % (check_expiration(p.expiration_date))
+            print "Attached Certificates:"
+            for c in p.certs:
+                print "\t%s %s" % (c.cn, check_expiration(c.validity))
+            for c in p.certs_expired:
+                print "\t%s %s" % (c.cn, check_expiration(c.validity))
+            #for d in p.udids:
+            #    print "\n\t%s" % (d)
+            continue
 
-            print p.appid
+        if args.appid and args.appid != p.appid:
+            logging.debug("%s: Doesn't match specified appid (%s)" % (p.uuid, p.appid))
+            continue
+
+        if args.production and p.development:
+            logging.warning("%s: Doesn't match environment (%s)" % (p.uuid, "pro"))
+            continue
+        elif not args.production and p.development == False:
+            logging.warning("%s: Doesn't match environment (%s)" % (p.uuid, "dev"))
+            continue
+
+        if not p.certs:
+            logging.warning("%s: No valid certificates found" % (p.uuid))
+            continue
+
+        if args.identities:
+            for c in p.certs:
+                if c.cn in args.identities:
+                    provisioning_profiles.append(p)
+                    break
+                else:
+                    logging.error("%s: Looks good, but no certificates match the codesigning identities available within the keychain." % (p.uuid))
+        else:
             provisioning_profiles.append(p)
-        except:
-            logging.error("Invalid provisioning profile: %s" % (profile))
 
-    for p in provisioning_profiles:
-        print p.uuid
+    # Make sure the wildcard provisioning profiles come last
+    for p in sorted(provisioning_profiles, key=lambda x: x.appid, reverse=args.wildcard):
+        for c in p.certs:
+            print '@'.join([p.uuid, p.name, c.cn])
